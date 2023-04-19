8dbb17702a38fe06faa1f07d5549835b75d7023a
Andrew <andrewdebyl@gmail.com>
2023-04-19 09:10:14 -0400

    changes

--- apple-clone/tailwind.config.js
+++ apple-clone/tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        body: ["Poppins", "sans-serif"]
+    
+      ],
+    
+      theme: {
+        extend: {},
       },
-    },
-  },
-  plugins: [],
-}
+      plugins: [],
+    }
